900bc2fd63fffcf6107ce878a50423e6d0ff9c3a
Enagendy <engagendy@emagile.com>
2026-04-05 21:54:05 +0400

    Improve Windows release packaging logs

--- build_release_windows.bat
+++ build_release_windows.bat
@@ -10,14 +10,21 @@ set "CHECKSUM_NAME=ULTRA_FORCE-windows-%RELEASE_VERSION%-sha256.txt"
 call "%ROOT_DIR%build_web_app.bat"
 if errorlevel 1 exit /b 1
 
-powershell -NoProfile -Command "if (Test-Path '%ROOT_DIR%dist\%ZIP_NAME%') { Remove-Item '%ROOT_DIR%dist\%ZIP_NAME%' -Force }; Compress-Archive -Path '%ROOT_DIR%dist\ULTRA_FORCE\*' -DestinationPath '%ROOT_DIR%dist\%ZIP_NAME%' -Force"
+echo [1/4] Packaging application folder
+powershell -NoProfile -Command "if (Test-Path '%ROOT_DIR%dist\%ZIP_NAME%') { Remove-Item '%ROOT_DIR%dist\%ZIP_NAME%' -Force }"
 if errorlevel 1 exit /b 1
 
+echo [2/4] Creating ZIP archive with 7-Zip
+7z a -tzip "%ROOT_DIR%dist\%ZIP_NAME%" "%ROOT_DIR%dist\ULTRA_FORCE\*" >nul
+if errorlevel 1 exit /b 1
+
+echo [3/4] Building Windows installer
 if exist "%ProgramFiles(x86)%\Inno Setup 6\ISCC.exe" (
   "%ProgramFiles(x86)%\Inno Setup 6\ISCC.exe" "%ROOT_DIR%installer_windows.iss"
   if errorlevel 1 exit /b 1
 )
 
+echo [4/4] Writing checksums
 powershell -NoProfile -Command "$files = @('%ROOT_DIR%dist\%ZIP_NAME%'); if (Test-Path '%ROOT_DIR%dist\%SETUP_NAME%') { $files += '%ROOT_DIR%dist\%SETUP_NAME%' }; Get-FileHash -Algorithm SHA256 $files | ForEach-Object { "" + $_.Hash.ToLower() + ' *' + [System.IO.Path]::GetFileName($_.Path) } | Set-Content '%ROOT_DIR%dist\%CHECKSUM_NAME%'"
 if errorlevel 1 exit /b 1
 
